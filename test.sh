c72099fb6146c6e167ba43fbc5e9b8eeb9cb2f09
M100ika <maxat.suieubayev@gmail.com>
2022-03-04 01:58:32 +0600

    first com

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "# Raspberry_scales" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin git@github.com:pcfkazatu/Raspberry_scales.git
+ git push -u origin основной
