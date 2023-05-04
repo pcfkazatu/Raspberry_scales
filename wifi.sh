f7f8511eba1068c9e71f1bf50c4812c6d3bdd833
pcfkazatu <100610907+pcfkazatu@users.noreply.github.com>
2023-05-04 22:27:34 +0600

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -2,7 +2,6 @@ cd /home/ubuntu/scales/
 mv /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init-orig.yaml
 cp 50-cloud-init.yaml /etc/netplan/
 
-route del default eth0
-route add default wlan0
-
 netplan --debug apply
+
+route del default eth0
