d3c50f416ed46eaec75a4111dc083f4197fffdef
pcfkazatu <100610907+pcfkazatu@users.noreply.github.com>
2023-05-04 19:51:33 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ apt install python3-pip
 
 pip3 install loguru
 
-mkdir /home/pi/arduino
-cd /home/pi/arduino/
+mkdir /home/ubuntu/arduino
+cd /home/ubuntu/arduino/
 wget https://downloads.arduino.cc/arduino-1.8.19-linuxarm.tar.xz
 tar -xf arduino-1.8.19-linuxarm.tar.xz
 mv arduino-1.8.19 /opt
@@ -12,3 +12,7 @@ mv arduino-1.8.19 /opt
 chmod a+rw /dev/ttyACM0
 
 timedatectl set-timezone Asia/Almaty
+
+cd /home/ubuntu/scales/
+mv /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init-orig.yaml
+cp 50-cloud-init.yam /etc/netplan/
