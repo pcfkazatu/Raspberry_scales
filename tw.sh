19d8002565628250bb552cd0769a887aec9d8199
pcfkazatu <100610907+pcfkazatu@users.noreply.github.com>
2023-05-06 21:05:19 +0600

    Update tw.sh

--- tw.sh
+++ tw.sh
@@ -1,10 +1,5 @@
-#Установка TeamViewer
-cd /home/ubuntu/Downloads/
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
-dpkg -i teamviewer-host_armhf.deb || true
-apt --fix-broken install  
-#teamviewer passwd californicatioN 
-# cd ${HOME}/Desktop
-echo "#########" >> teamviewer_info.txt
-teamviewer info >> teamviewer_info.txt
-echo "TeamViewer done"
+dpkg -i teamviewer-host_armhf.deb
+apt-get install -f
+teamviewer daemon start
+teamviewer setup
